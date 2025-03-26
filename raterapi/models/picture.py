f3a472494bc4af73b421635ed4c6251747c0f12e
Sabra Kozlina <sjkozlina@gmail.com>
2025-03-26 15:34:28 -0600

    update

--- raterapi/models/picture.py
+++ raterapi/models/picture.py
@@ -5,4 +5,4 @@ from .game import Game
 class Picture(models.Model):
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    image = models.ImageField()
+    image = models.CharField(max_length=250)
